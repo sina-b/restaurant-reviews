c17eacf9e9f01a2d641d6fd54beafefdc45c3014
Sina Brandstetter <brandstetter.sina@gmail.com>
2020-05-15 19:05:13 +0200

    Add responsiveness, accessibility and service worker

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1Ijoic2luYWIiLCJhIjoiY2thOGNibWI5MGJzMzJzcDRyYzJtdHFqYyJ9.zwKhyHVOV719_-1NsgU_tA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
---
+++ js/sw.js
@@ -0,0 +1,61 @@
+let cacheFiles=[
+  '/',
+  '/restaurant.html',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/js/dbhelper.js',
+  '/css/styles.css',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
+];
+
+let restaurantCache = 'restaurant-v1';
+
+self.addEventListener('install', function(event) {
+  console.log('Install service worker');
+  event.waitUntil(
+    caches.open(restaurantCache).then(function(cache) {
+      return cache.addAll(cacheFiles);
+    }).catch(function(error){
+      console.log(error);
+    })
+  );
+});
+
+
+self.addEventListener('activate', function(event) {
+  console.log('Activate service worker');
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('restaurant-') &&
+                 cacheName != restaurantCache;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+
+self.addEventListener('fetch',function(event){
+  console.log('Hijack requests');
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      if(response) return response;
+      return fetch(event.request);
+    }).catch(function(err){
+          console.log(err);
+      })
+  );
+});
